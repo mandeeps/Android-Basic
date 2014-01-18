d44a8d046cff0f260515605865e069c6ec479188
Mandeep Shergill <mandeep@server.fake>
2014-01-18 18:49:40 -0500

    Coursera Angrave Week 5 lessons

---
+++ Angrave-Week5-WebContent/src/com/example/webcontent/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.webcontent;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	WebView web;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		web = (WebView) findViewById(R.id.webView1);
+		web.getSettings().setBuiltInZoomControls(true);
+		web.getSettings().setJavaScriptEnabled(true);
+		web.loadUrl("file:///android_asset/index.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack()) {
+			web.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+}

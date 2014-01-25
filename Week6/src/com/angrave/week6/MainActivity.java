c260dd493fd8b8ebb2a467e3952e3f48b9a1cf45
Mandeep Shergill <mandeep@server.fake>
2014-01-25 16:18:41 -0500

    animation and try/catch block

--- Week6/src/com/angrave/week6/MainActivity.java
+++ Week6/src/com/angrave/week6/MainActivity.java
@@ -2,8 +2,11 @@ package com.angrave.week6;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -32,7 +35,30 @@ public class MainActivity extends Activity {
 	
 	public void launch(View button) {
 		String comments = mComments.getText().toString();
-		Toast.makeText(this.getApplicationContext(), comments, Toast.LENGTH_LONG).show();
+		String email = mEmail.getText().toString();
+		String phone = mPhone.getText().toString();
+		String name = mName.getText().toString();
+		int position = 0;
+
+		try {
+			position = email.indexOf("@");
+			if (position == -1) {
+				Toast.makeText(this.getApplicationContext(), "Invalid email!", Toast.LENGTH_LONG).show();
+				mEmail.requestFocus();
+				return;
+			}
+		}
+		catch (Exception e) {
+			Log.d("exception", e.toString());
+		}
+		
+		String username = email.substring(0, position);
+		String thanks = "Thank you " + username + "!";
+		
+		Toast.makeText(this.getApplicationContext(), thanks, Toast.LENGTH_LONG).show();
+		
+		Animation anim = AnimationUtils.makeOutAnimation(this, true);
+		button.startAnimation(anim);
 		button.setVisibility(View.INVISIBLE);
 		Toast.makeText(this.getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
 	}
